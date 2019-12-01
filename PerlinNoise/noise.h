c0e35b185cf0d88a6699bf9dcaac2bba500e1efb
Ben Jollymore <45041369+benjollymore@users.noreply.github.com>
2019-12-01 03:10:36 -0400

    bug breaking things at the moment in fbm function

--- PerlinNoise/noise.h
+++ PerlinNoise/noise.h
@@ -77,7 +77,7 @@ R32FTexture* fBm2DTexture() {
 			int J = j;
 			for (int k = 0; k < octaves; ++k) {
 				/// TODO: Get perlin noise at I,J, add offset, multiply by proper term and add to noise_data
-				value += *perlin2D(I, J) * exponent_array[k];
+				//value += *perlin2D(I, J) * exponent_array[k];
 				noise_data[i + j * height] += 0;
 
 				///--- Point to sample at next octave
